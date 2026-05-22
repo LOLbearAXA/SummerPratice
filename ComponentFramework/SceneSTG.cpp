dc8d83a62119a2d6f259b15627e592c5270042ce
LOLbearAXA <jackyy3767@gmail.com>
2026-05-22 15:01:19 -0400

    Set Up

---
+++ ComponentFramework/SceneSTG.cpp
@@ -0,0 +1,100 @@
+#include <glew.h>
+#include <iostream>
+#include <SDL.h>
+#include <SDL3/SDL_events.h>
+#include "SceneSTG.h"
+#include <MMath.h>
+#include "Debug.h"
+#include "Mesh.h"
+#include "Shader.h"
+#include "Body.h"
+#include "Texture.h"
+#include <QMath.h>
+
+SceneSTG::SceneSTG() :sphere{ nullptr }, shader{ nullptr }, mesh{ nullptr },
+drawInWireMode{ false } {
+	Debug::Info("Created Scene0: ", __FILE__, __LINE__);
+}
+
+SceneSTG::~SceneSTG() {
+	Debug::Info("Deleted Scene0: ", __FILE__, __LINE__);
+}
+
+bool SceneSTG::OnCreate() {
+	Debug::Info("Loading assets SceneSTG: ", __FILE__, __LINE__);
+
+	return true;
+}
+
+void SceneSTG::OnDestroy() {
+	Debug::Info("Deleting assets Scene0: ", __FILE__, __LINE__);
+	sphere->OnDestroy();
+	delete sphere;
+
+	sphere2->OnDestroy();
+	delete sphere2;
+
+	plane1->OnDestroy();
+	delete plane1;
+
+	mesh->OnDestroy();
+	delete mesh;
+
+	planeMesh->OnDestroy();
+	delete planeMesh;
+
+	skullMesh->OnDestroy();
+	delete skullMesh;
+
+	shader->OnDestroy();
+	delete shader;
+
+	delete earthTexture;
+
+	delete moonTexture;
+
+	delete marioTexture;
+}
+
+void SceneSTG::HandleEvents(const SDL_Event& sdlEvent) {
+	switch (sdlEvent.type) {
+	case SDL_EVENT_KEY_DOWN:
+		switch (sdlEvent.key.scancode) {
+		case SDL_SCANCODE_W:
+			drawInWireMode = !drawInWireMode;
+			break;
+		default:
+			break;
+		}
+		break;
+	}
+}
+
+void SceneSTG::Update(const float deltaTime) {
+	static float totalTime = 0.0f;
+	totalTime += deltaTime;
+
+}
+
+void SceneSTG::Render() const {
+	glEnable(GL_DEPTH_TEST);
+
+	/// Set the background color then clear the screen
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	if (drawInWireMode) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+	else {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+
+	glUseProgram(shader->GetProgram()); // turn on shader
+	glUniformMatrix4fv(shader->GetUniformID("projectionMatrix"), 1, GL_FALSE, projectionMatrix);
+	glUniformMatrix4fv(shader->GetUniformID("viewMatrix"), 1, GL_FALSE, viewMatrix);
+	glUniform3fv(shader->GetUniformID("lightPos"), 1, lightPos);
+
+	glBindTexture(GL_TEXTURE_2D, 0); // UNDIND TEXTURE (Don't need to unbind everytime)
+	glUseProgram(0); // TURN OFF THE SHADER
+}
